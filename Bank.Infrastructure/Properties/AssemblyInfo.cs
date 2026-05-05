3af7a54343dce1bdd848507dcb87024b5af1e4cf
Madan.patakota <madan.patakota@gmail.com>
2026-05-05 09:12:01 +0530

    done

--- Bank.Infrastructure/Properties/AssemblyInfo.cs
+++ Bank.Infrastructure/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DependencyInjection")]
+[assembly: AssemblyTitle("Bank.Infrastructure")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DependencyInjection")]
+[assembly: AssemblyProduct("Bank.Infrastructure")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("93cab1b7-8aa3-4af1-8d69-d121a5aa5213")]
+[assembly: Guid("6de29029-a8cb-4105-b1fd-c7b2e5e94906")]
 
 // Version information for an assembly consists of the following four values:
 //
